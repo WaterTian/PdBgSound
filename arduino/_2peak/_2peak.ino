c4e114efd88be52a3cdfdde1f3b9001d6f15adf0
changewater <changewater@qq.com>
2017-04-20 15:11:14 +0800

    git

--- arduino/_2peak/_2peak.ino
+++ arduino/_2peak/_2peak.ino
@@ -2,21 +2,28 @@
 void setup ()
 {
   Serial.begin(9600);
-  pinMode(A15, INPUT);
-  pinMode(A14, INPUT);
-  pinMode(A13, INPUT);
-  pinMode(A12, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
 }
 void loop () {
-  int a15Vle = digitalRead(A15);
-  int a14Vle = digitalRead(A14);
-  int a13Vle = digitalRead(A13);
-  int a12Vle = digitalRead(A12);
+  int a0Vle = digitalRead(A0);
+  int a1Vle = digitalRead(A1);
+  int a2Vle = digitalRead(A2);
+  int a3Vle = digitalRead(A3);
+  int a4Vle = digitalRead(A4);
+  int a5Vle = digitalRead(A5);
   
-  Serial.print(a12Vle);
-  Serial.print(a13Vle);
-  Serial.print(a14Vle);
-  Serial.println(a15Vle);
+  
+  Serial.print(a0Vle);
+  Serial.print(a1Vle);
+  Serial.print(a2Vle);
+  Serial.print(a3Vle);
+  Serial.print(a4Vle);
+  Serial.println(a5Vle);
 }
 
 
